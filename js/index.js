218950cab1063ed961b60b6e0bdeae9e8b657675
Jeriah Bowers <jeriah97bowers@yahoo.com>
2021-02-12 23:25:31 -0500

    finishing navigation functionality

--- js/index.js
+++ js/index.js
@@ -5,8 +5,15 @@ const navigationController = (function() {
     const _dropdown = document.querySelector('.navigation__dropdown');
 
     _button.addEventListener('click', () => {
-        _dropdown.style.visibility = "visible";
-        _dropdown.style.opacity = "1";
+        if(window.getComputedStyle(_dropdown).visibility === 'hidden') {
+            _dropdown.style.visibility = "visible";
+            _dropdown.style.opacity = "1";
+            _button.childNodes[1].classList.replace('fa-bars', 'fa-times');
+        } else {
+            _dropdown.style.visibility = "hidden";
+            _dropdown.style.opacity = "0";
+            _button.childNodes[1].classList.replace('fa-times', 'fa-bars');
+        }
     });
 
 }());
