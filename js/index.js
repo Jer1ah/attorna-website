93e7de0f763f9e8d5617bb19df8e37b5956c81fc
Jeriah Bowers <jeriah97bowers@yahoo.com>
2021-02-16 23:12:49 -0500

    adding and styling contact section

--- js/index.js
+++ js/index.js
@@ -39,10 +39,14 @@ const parallaxScrollController = (function() {
 
 }());
 
-$('.testimonials__list').slick({
-    dots: true,
-    prevArrow: false,
-    nextArrow: false,
-    autoplay: true,
-    autoplaySpeed: 3500
-});
+
+// adding slick slider
+const slickSliderController = (function() {
+    $('.testimonials__list').slick({
+        dots: true,
+        prevArrow: false,
+        nextArrow: false,
+        autoplay: true,
+        autoplaySpeed: 3500
+    });
+}());
